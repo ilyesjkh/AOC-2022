220bde2368a57bd0840d4fa0a63bf8c39aaad168
ilyesjkh <abdellouli.ilyes@gmail.com>
2022-12-03 15:33:34 +0100

    Second day

---
+++ day2/day2.js
@@ -0,0 +1,96 @@
+const { readFileSync, promises: fsPromises } = require('fs');
+
+function partOne(filename){
+    const RPSClashes = readFileSync(filename, 'utf-8').split("\n");
+    console.log(RPSClashes);
+    let score = 0;
+    const outcomePoints = {win:6, draw:3, loss:0};
+    const shapesPoints = {rock:1, paper:2, scissors:3};
+    for(let i = 0; i<RPSClashes.length;i++){
+        let opponnentsMove = RPSClashes[i].split(" ")[0];
+        let myMove = RPSClashes[i].split(" ")[1];
+        if(opponnentsMove == "A"){
+            if(myMove == "X"){
+                score += outcomePoints.loss + shapesPoints.scissors;
+            }
+            else if(myMove == "Y"){
+                score += outcomePoints.draw + shapesPoints.rock;
+            }
+            else if(myMove == "Z"){
+                score += outcomePoints.win + shapesPoints.paper;
+            }
+        }
+        else if(mMove == "Y"){
+            if(opponnentsMove == "A"){
+                score += outcomePoints.win + shapesPoints.paper;
+            }
+            else if(opponnentsMove == "B"){
+                score += outcomePoints.draw + shapesPoints.paper;
+            }
+            else if(opponnentsMove == "C"){
+                score += outcomePoints.loss + shapesPoints.paper;
+            }
+        }
+        else if(myMove == "Z"){
+            if(opponnentsMove == "A"){
+                score += outcomePoints.loss + shapesPoints.scissors;
+            }
+            else if(opponnentsMove == "B"){
+                score += outcomePoints.win + shapesPoints.scissors;
+            }
+            else if(opponnentsMove == "C"){
+                score += outcomePoints.draw + shapesPoints.scissors;
+            }
+        }
+    }
+    return score;
+}
+
+function partTwo(filename){
+    const RPSClashes = readFileSync(filename, 'utf-8').split("\n");
+    console.log(RPSClashes);
+    let score = 0;
+    const outcomePoints = {win:6, draw:3, loss:0};
+    const shapesPoints = {rock:1, paper:2, scissors:3};
+    for(let i = 0; i<RPSClashes.length;i++){
+        let opponnentsMove = RPSClashes[i].split(" ")[0];
+        let myMove = RPSClashes[i].split(" ")[1];
+        if(myMove == "X"){
+            if(opponnentsMove == "A"){
+                score += outcomePoints.loss + shapesPoints.scissors;
+            }
+            else if(opponnentsMove == "B"){
+                score += outcomePoints.loss + shapesPoints.rock;
+            }
+            else if(opponnentsMove == "C"){
+                score += outcomePoints.loss + shapesPoints.paper;
+            }
+        }
+        else if(myMove == "Y"){
+            if(opponnentsMove == "A"){
+                score += outcomePoints.draw + shapesPoints.rock;
+            }
+            else if(opponnentsMove == "B"){
+                score += outcomePoints.draw + shapesPoints.paper;
+            }
+            else if(opponnentsMove == "C"){
+                score += outcomePoints.draw + shapesPoints.scissors;
+            }
+        }
+        else if(myMove == "Z"){
+            if(opponnentsMove == "A"){
+                score += outcomePoints.win + shapesPoints.paper;
+            }
+            else if(opponnentsMove == "B"){
+                score += outcomePoints.win + shapesPoints.scissors;
+            }
+            else if(opponnentsMove == "C"){
+                score += outcomePoints.win + shapesPoints.rock;
+            }
+        }
+        
+    }
+    return score;
+}
+
+console.log(partTwo("day2_input.txt"));
